6e13b77fd407e8c41272e749bbacd447bb0b85e5
Jack Thacker <jackthacker89@gmail.com>
2022-04-05 19:20:50 +0100

    Added js file, added initial variables and formatted the text for the calc screen display

---
+++ script.js
@@ -0,0 +1,7 @@
+let screenVisible;
+let screenHidden;
+
+const symbolPlus = screenHidden + screenVisible;
+const symbolMinus = screenHidden - screenVisible;
+const symbolMultiply = screenHidden * screenVisible;
+const symbolDivide = screenHidden / screenVisible;
