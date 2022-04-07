2fc329d93bc5539b8c6818e0af75b2fda82dbe22
Jack Thacker <jackthacker89@gmail.com>
2022-04-07 17:44:07 +0100

    Added script to handle the cancel button

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-let screenVisible;
-let screenHidden;
+// Variables which determine the properties of the display and the current calculation
+let displayVisible;
+let displayHidden;
+const calcDisplay = document.querySelector("#display");
 
-const symbolPlus = screenHidden + screenVisible;
-const symbolMinus = screenHidden - screenVisible;
-const symbolMultiply = screenHidden * screenVisible;
-const symbolDivide = screenHidden / screenVisible;
+let symbolNeutral;
+let currentSymbol;
+const symbolPlus = displayHidden + displayVisible;
+const symbolMinus = displayHidden - displayVisible;
+const symbolMultiply = displayHidden * displayVisible;
+const symbolDivide = displayHidden / displayVisible;
+
+// Cancels the calculation and resets the display to zero
+const handleCancel = () => {
+  calcDisplay.innerText = "0";
+  displayVisible = 0;
+  displayHidden = 0;
+  currentSymbol = symbolNeutral;
+};
+
+// Handles the entry of symbols (i.e., queues up the calculation)
+// const btnSymbols = document.querySelectorAll(".calc__btn--symbol");
+// btnSymbols.forEach((symbol) => {
+//   symbol.addEventListener("click", (event) => {
+//     displayHidden = displayHidden += displayVisible;
+//     switch (event.target.innerText) {
+//       case "+":
+//         currentSymbol = symbolPlus;
+//         break;
+//       case "-":
+//         currentSymbol = symbolMinus;
+//         break;
+//       case "×":
+//         currentSymbol = symbolMultiply;
+//         break;
+//       case "÷":
+//         currentSymbol = symbolDivide;
+//         break;
+//     }
+//     console.log(currentSymbol);
+//   });
+// });
