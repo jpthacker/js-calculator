0e817018d5ab13aa244d28936af72e9692fd735c
Jack Thacker <jackthacker89@gmail.com>
2022-04-08 13:15:44 +0100

    Added placeholder script for number buttons but decimals do not work

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // Variables which determine the properties of the display and the current calculation
-let displayVisible = 1;
-let displayHidden = 3;
+let displayValue;
+let totalValue;
 const calcDisplay = document.querySelector("#display");
-
-let symbolNeutral;
+let symbolNeutral = "n";
 let currentSymbol;
-let symbolPlus;
-let symbolMinus;
-let symbolMultiply;
-let symbolDivide;
+let symbolPlus = "p";
+let symbolMinus = "mi";
+let symbolMultiply = "mu";
+let symbolDivide = "d";
 
 // Cancels the calculation and resets the display to zero
 const handleCancel = () => {
   calcDisplay.innerText = "0";
-  displayVisible = 0;
-  displayHidden = 0;
+  displayValue = 0;
+  totalValue = 0;
   currentSymbol = symbolNeutral;
 };
 
@@ -22,21 +21,88 @@ const handleCancel = () => {
 const btnSymbols = document.querySelectorAll(".calc__btn--symbol");
 btnSymbols.forEach((symbol) => {
   symbol.addEventListener("click", (event) => {
-    displayHidden = displayHidden += displayVisible;
-    switch (event.target.innerText) {
-      case "+":
+    displayValue = parseFloat(calcDisplay.innerText);
+    if (totalValue === 0) {
+      totalValue = displayValue;
+    }
+    switch (event.target.id) {
+      case "plus":
         currentSymbol = symbolPlus;
         break;
-      case "-":
+      case "minus":
         currentSymbol = symbolMinus;
         break;
-      case "×":
+      case "multiply":
         currentSymbol = symbolMultiply;
         break;
-      case "÷":
+      case "divide":
         currentSymbol = symbolDivide;
         break;
     }
-    console.log(currentSymbol);
   });
 });
+
+// Handles the entry of numerical buttons and ensures continued calculations
+const btnNumbers = document.querySelectorAll(".calc__btn--num");
+btnNumbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    if (currentSymbol === symbolNeutral && displayValue != 0) {
+      calcDisplay.innerText = calcDisplay.innerText + event.target.innerText;
+    } else {
+      calcDisplay.innerText = event.target.innerText;
+      displayValue = parseFloat(calcDisplay.innerText);
+      switch (currentSymbol) {
+        case symbolPlus:
+          totalValue = totalValue += displayValue;
+          break;
+        case symbolMinus:
+          totalValue = totalValue -= displayValue;
+          break;
+        case symbolMultiply:
+          totalValue = totalValue * displayValue;
+          break;
+        case symbolDivide:
+          totalValue = totalValue / displayValue;
+          break;
+      }
+    }
+    console.log(totalValue);
+  });
+});
+
+// Handles the equals symbol
+
+// Sources the numarical value of number buttons
+// let currentInteger;
+// switch (event.target.id) {
+//   case "zero":
+//     currentInteger = 0;
+//     break;
+//   case "one":
+//     currentInteger = 1;
+//     break;
+//   case "two":
+//     currentInteger = 2;
+//     break;
+//   case "three":
+//     currentInteger = 3;
+//     break;
+//   case "four":
+//     currentInteger = 4;
+//     break;
+//   case "five":
+//     currentInteger = 5;
+//     break;
+//   case "six":
+//     currentInteger = 6;
+//     break;
+//   case "seven":
+//     currentInteger = 7;
+//     break;
+//   case "8":
+//     currentInteger = 8;
+//     break;
+//   case "9":
+//     currentInteger = 9;
+//     break;
+// }
