ccb0af775b3266ccd912e0cce0506f13f92fe2c8
Jack Thacker <jackthacker89@gmail.com>
2022-04-08 09:14:47 +0100

    Added placeholder script for click events for symbols (discounting equals)

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Variables which determine the properties of the display and the current calculation
-let displayVisible;
-let displayHidden;
+let displayVisible = 1;
+let displayHidden = 3;
 const calcDisplay = document.querySelector("#display");
 
 let symbolNeutral;
 let currentSymbol;
-const symbolPlus = displayHidden + displayVisible;
-const symbolMinus = displayHidden - displayVisible;
-const symbolMultiply = displayHidden * displayVisible;
-const symbolDivide = displayHidden / displayVisible;
+let symbolPlus;
+let symbolMinus;
+let symbolMultiply;
+let symbolDivide;
 
 // Cancels the calculation and resets the display to zero
 const handleCancel = () => {
@@ -19,24 +19,24 @@ const handleCancel = () => {
 };
 
 // Handles the entry of symbols (i.e., queues up the calculation)
-// const btnSymbols = document.querySelectorAll(".calc__btn--symbol");
-// btnSymbols.forEach((symbol) => {
-//   symbol.addEventListener("click", (event) => {
-//     displayHidden = displayHidden += displayVisible;
-//     switch (event.target.innerText) {
-//       case "+":
-//         currentSymbol = symbolPlus;
-//         break;
-//       case "-":
-//         currentSymbol = symbolMinus;
-//         break;
-//       case "×":
-//         currentSymbol = symbolMultiply;
-//         break;
-//       case "÷":
-//         currentSymbol = symbolDivide;
-//         break;
-//     }
-//     console.log(currentSymbol);
-//   });
-// });
+const btnSymbols = document.querySelectorAll(".calc__btn--symbol");
+btnSymbols.forEach((symbol) => {
+  symbol.addEventListener("click", (event) => {
+    displayHidden = displayHidden += displayVisible;
+    switch (event.target.innerText) {
+      case "+":
+        currentSymbol = symbolPlus;
+        break;
+      case "-":
+        currentSymbol = symbolMinus;
+        break;
+      case "×":
+        currentSymbol = symbolMultiply;
+        break;
+      case "÷":
+        currentSymbol = symbolDivide;
+        break;
+    }
+    console.log(currentSymbol);
+  });
+});
