210a6d205b12997cf59bab02fc5296b60041c38b
Jack Thacker <jackthacker89@gmail.com>
2022-04-12 11:43:39 +0100

    Fixed some logical errors in the behaviour of equals button and calculations

--- script.js
+++ script.js
@@ -1,52 +1,65 @@
 // Variables which determine the properties of the display and the current calculation
-let displayValue;
-let totalValue = 0;
+let displayValue = 0;
+let totalValue = "t";
 const calcDisplay = document.querySelector("#display");
+let numberStatus;
 let symbolNeutral = "n";
-let currentSymbol;
+let currentSymbol = symbolNeutral;
 let symbolPlus = "p";
 let symbolMinus = "m";
 let symbolMultiply = "x";
 let symbolDivide = "d";
-let lastSymbol;
-let result;
+let result = "r";
 
 // Cancels the calculation and resets the display to zero
 const handleCancel = () => {
   calcDisplay.innerText = "0";
   displayValue = 0;
-  totalValue = 0;
-  result = 1;
+  totalValue = "t";
   currentSymbol = symbolNeutral;
+  result = "r";
 };
 
-// Handles the entry of symbols (i.e., queues up the calculation)
-const btnSymbols = document.querySelectorAll(".calc__btn--symbol");
-btnSymbols.forEach((symbol) => {
-  symbol.addEventListener("click", (event) => {
-    displayValue = parseFloat(calcDisplay.innerText);
-    if (totalValue === 0) {
-      totalValue = displayValue;
-    }
-    switch (event.target.id) {
-      case "plus":
-        currentSymbol = symbolPlus;
-        break;
-      case "minus":
-        currentSymbol = symbolMinus;
-        break;
-      case "multiply":
-        currentSymbol = symbolMultiply;
-        break;
-      case "divide":
-        currentSymbol = symbolDivide;
-        break;
-    }
-  });
-});
+// Handles number entry
+const handleNumberEntry = (event) => {
+  if (
+    numberStatus === "add" &&
+    displayValue != 0 &&
+    calcDisplay.innerText != totalValue
+  ) {
+    calcDisplay.innerText = calcDisplay.innerText + event.target.innerText;
+  } else {
+    calcDisplay.innerText = event.target.innerText;
+  }
+  displayValue = parseFloat(calcDisplay.innerText);
+  numberStatus = "add";
+};
+
+// Handles symbol entry
+const handleSymbolEntry = (event) => {
+  switch (event.target.id) {
+    case "plus":
+      currentSymbol = symbolPlus;
+      break;
+    case "minus":
+      currentSymbol = symbolMinus;
+      break;
+    case "multiply":
+      currentSymbol = symbolMultiply;
+      break;
+    case "divide":
+      currentSymbol = symbolDivide;
+      break;
+  }
+};
 
 // Runs the calculation depending on the symbol pressed
 const handleCalculation = (symbol) => {
+  numberStatus = "replace";
+  result = "r";
+  if (totalValue === "t") {
+    totalValue = displayValue;
+  }
   switch (symbol) {
     case symbolPlus:
       totalValue = totalValue += displayValue;
@@ -63,33 +76,31 @@ const handleCalculation = (symbol) => {
   }
 };
 
-// Handles the entry of numerical buttons & ensures continued calculations
+// Handles the entry of numerical buttons via click events
 const btnNumbers = document.querySelectorAll(".calc__btn--num");
 btnNumbers.forEach((number) => {
   number.addEventListener("click", (event) => {
-    if (
-      currentSymbol === symbolNeutral &&
-      displayValue != 0 &&
-      calcDisplay.innerText != totalValue
-    ) {
-      calcDisplay.innerText = calcDisplay.innerText + event.target.innerText;
-    } else {
-      calcDisplay.innerText = event.target.innerText;
-      displayValue = parseFloat(calcDisplay.innerText);
+    handleNumberEntry(event);
+  });
+});
+
+// Handles the entry of symbols via clicks
+const btnSymbols = document.querySelectorAll(".calc__btn--symbol");
+btnSymbols.forEach((symbol) => {
+  symbol.addEventListener("click", (event) => {
+    if (result != totalValue) {
       handleCalculation(currentSymbol);
     }
-    lastSymbol = currentSymbol;
-    currentSymbol = symbolNeutral;
+    handleSymbolEntry(event);
   });
 });
 
 // Displays the result on the screen & controls the behaviour of equals button
 const handleResult = () => {
-  if (result === totalValue) {
-    handleCalculation(lastSymbol);
-  }
+  handleCalculation(currentSymbol);
   calcDisplay.innerText = totalValue;
   result = totalValue;
+  numberStatus = "replace";
 };
 
 // Sources the numarical value of number buttons
